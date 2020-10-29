7989080e131c1454e0e17305e67905f326eba25c
wael harrath <waelharrath@gmail.com>
2020-10-29 10:10:46 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
@@ -6,6 +6,24 @@ import NavigBar from "./components/NavigBar";
 function App() {
   const [inputTxt, setInputTxt] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <>
       <NavigBar />
@@ -14,8 +32,13 @@ function App() {
         todos={todos}
         setTodos={setTodos}
         setInputTxt={setInputTxt}
+        setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,23 @@
 import React from "react";
 
-function Form({ setInputTxt, inputTxt, todos, setTodos }) {
+function Form({ setInputTxt, inputTxt, todos, setTodos,setStatus }) {
   const inputTxtChanger = (event) => {
     setInputTxt(event.target.value);
   };
   const toDoChanger = (e) => {
     e.preventDefault();
-    setTodos([
+    if (inputTxt ==="")
+    {alert ("can't add an empty TODO !!")}
+    else
+    {setTodos([
       ...todos,
       { text: inputTxt, completed: false, id: Math.random() * 1000 },
-    ]);
+    ]);}
     setInputTxt("");
   };
+  const handleStatus =(e)=>{
+    setStatus(e.target.value);
+  }
   return (
     <form>
       <input
@@ -23,7 +29,15 @@ function Form({ setInputTxt, inputTxt, todos, setTodos }) {
       <button onClick={toDoChanger} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
+      <div className="select">
+      <select onChange={handleStatus}  name="todos" className="filter-todo">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="uncompleted">Uncompleted</option>
+      </select>
+      </div>
     </form>
+
   );
 }
 export default Form;
